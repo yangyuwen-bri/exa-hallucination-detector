4f99ba7785465deb0ebbecf039568f34ff5c6853
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-13 02:35:58 +0530

    optimised for mobile spacing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import FactChecker from '../components/FactChecker';
 
 export default function Home() {
   return (
-    <main className="flex relative min-h-screen flex-col items-center justify-center p-4">
+    <main className="flex relative min-h-screen flex-col items-center justify-center md:p-4">
 
       {/* background grid design texture code */}
       <div className="absolute inset-0 -z-0 h-full w-full bg-[radial-gradient(#80808060_1px,transparent_1px)] [background-size:30px_30px]"></div>
