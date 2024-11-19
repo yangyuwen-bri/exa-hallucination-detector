f4b49756b11b8d3da077f2725752c7abd83f47eb
elizabethtrykin <72474571+elizabethtrykin@users.noreply.github.com>
2024-11-19 14:10:25 -0800

    Add Next.js config for /hallucination-detector base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+//remove the below NextConfig when deploying
+const nextConfig = {
+    basePath: "/hallucination-detector",
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["demo.exa.ai"],
+        allowedForwardedHosts: ["demo.exa.ai"],
+      },
+    },
+  };
+  
+  export default nextConfig;
