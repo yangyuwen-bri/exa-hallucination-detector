b835fee4310fa19e457e80fa07753629e4d31d1c
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-20 02:32:30 +0530

    padding change

--- components/ui/TagPill.tsx
+++ components/ui/TagPill.tsx
@@ -7,7 +7,7 @@ interface TagPillProps {
 
 const TagPill = ({ content, icon }: TagPillProps) => {
   return (
-    <div className="inline-flex items-center px-3 py-1.5 rounded-full bg-brand-fainter text-brand-default text-sm break-all">
+    <div className="inline-flex items-center px-3 py-2 rounded-full bg-brand-fainter text-brand-default text-sm break-all">
       <div className="flex items-center min-w-0">
         {icon && <span className="mr-2 flex-shrink-0">{icon}</span>}
         <span className="truncate">{content}</span>
