2d24fc57f7cfdb013cf99ee5e90b4f08abef0b17
elizabethtrykin <72474571+elizabethtrykin@users.noreply.github.com>
2024-11-19 17:40:47 -0800

    Setup redirect from factchecker-exa.vercel.app

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+//redirects factchecker-exa.vercel.app to demo.exa.ai/hallucination-detector
+export function middleware(request: NextRequest) {
+  if (request.headers.get('host') === 'factchecker-exa.vercel.app') {
+    return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
+      status: 301
+    })
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: '/:path*'
+} 
