2fe6cfa925efc5e7f1fba42e04aef59413399bd1
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-29 22:22:13 +0530

    uncommenting the middleware + adding code to nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 //remove the below NextConfig when deploying
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/hallucination-detector",
+    experimental: {
+      serverActions: {
+        allowedOrigins: ["demo.exa.ai"],
+        allowedForwardedHosts: ["demo.exa.ai"],
+      },
+    },
+  };
   
 export default nextConfig;
