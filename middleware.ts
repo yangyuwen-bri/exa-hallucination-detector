db9d1d8204511e1054bc318f6baa9c94d1968e09
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-29 12:58:00 +0530

    removed factchecker-exa.vercel.app url routing

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-//redirects factchecker-exa.vercel.app to demo.exa.ai/hallucination-detector
+//redirects exa-hallucination-detector.vercel.app to demo.exa.ai/hallucination-detector
 export function middleware(request: NextRequest) {
   if (request.headers.get('host') === 'exa-hallucination-detector.vercel.app') {
-    return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
-      status: 301
-    })
-  }
-  if (request.headers.get('host') === 'factchecker-exa.vercel.app') {
     return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
       status: 301
     })
