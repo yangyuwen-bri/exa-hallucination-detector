b22c52430f681ac4c8e0029bf1c908fac7e20e03
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-29 12:53:31 +0530

    updated middleware for exa-hallucination-detector.vercel.app url

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,11 @@ import type { NextRequest } from 'next/server'
 
 //redirects factchecker-exa.vercel.app to demo.exa.ai/hallucination-detector
 export function middleware(request: NextRequest) {
+  if (request.headers.get('host') === 'exa-hallucination-detector.vercel.app') {
+    return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
+      status: 301
+    })
+  }
   if (request.headers.get('host') === 'factchecker-exa.vercel.app') {
     return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
       status: 301
