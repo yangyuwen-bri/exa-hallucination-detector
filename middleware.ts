7580172c0a82e217f9981a5654fd903c09b6310a
Ishan Goswami <ishangoswami315@gmail.com>
2024-11-29 22:11:21 +0530

    reverted to factchecker-exa.vercel.app

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-//redirects exa-hallucination-detector.vercel.app to demo.exa.ai/hallucination-detector
+//redirects factchecker-exa.vercel.app to demo.exa.ai/hallucination-detector
 export function middleware(request: NextRequest) {
-  if (request.headers.get('host') === 'exa-hallucination-detector.vercel.app') {
+  if (request.headers.get('host') === 'factchecker-exa.vercel.app') {
     return NextResponse.redirect('https://demo.exa.ai/hallucination-detector', {
       status: 301
     })
   }
   return NextResponse.next()
 }
-
 export const config = {
   matcher: '/:path*'
 } 
