d29406a28417cbd1a02049fc53fe84e1ce2d2992
Ishan Goswami <ishangoswami315@gmail.com>
2024-12-10 20:02:17 +0530

    updated footers

--- components/FactChecker.tsx
+++ components/FactChecker.tsx
@@ -291,7 +291,7 @@ export default function FactChecker() {
         <div className="max-w-md mx-auto">
           <p className="text-md text-center text-gray-600">
             <Link 
-              href="https://github.com/exa-labs/exa-hallucination-detector" 
+              href="https://github.com/exa-labs/hallucination-detector" 
               target="_blank"
               className="underline cursor-pointer"
             >
